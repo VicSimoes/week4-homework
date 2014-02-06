3c901dbf951b2a58b51af7152a8780b09a7132e5
VicSimoes <victor.simoes@gmail.com>
2014-02-06 10:25:57 -0600

    commit 2614

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
-#class WeatherController < ActionController::
-
-   #def search
-   #end
-
-    #def conditions
-     # @city = params[:city]
-    # @url = URI.escape(http://api.openweathermap.org/data)
-   #   @json_data = open(@url).read
-  #    render :text => "Hello There!"
- #   end
-
-#end
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,14 +2,17 @@ require 'open-uri'
 
 class WeatherController < ApplicationController
 
-   def search
-   end
+  def search
+  end
 
-    def conditions
-      @city = params[:city]
-      @url = URI.escape("http://api.openweathermap.org/data")
-      @json_data = open(@url).read
-      render :text => "Hello There!"
-    end
+  def conditions
+    @city = params[:city]
+    @url = URI.escape("http://api.openweathermap.org/data/2.5/weather?q=#{@city}&units=imperial")
+    @json_data = open(@url).read
+    @data = JSON.parse(@json_data)
+  end
 
 end
+
+#Route, Contoller, Action, View
+#ruby string, hash, array
